32b8b0211c6e533a632c6ccc09f3552a882b7266
lc <631979719@qq.com>
2013-07-11 15:20:18 +0800

    add code liucong

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test1::Application.config.secret_token = 'b34e1ba2d33881629214e9ba250f224bb1403a72502971d7c871fdaaa391d3caf5efbcf08365ee7c7e490504ff17c5b385e7039c0d133575d0705810e0a11f2e'
